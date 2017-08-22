b3cb0166ad4890d26b7197ac988c2b47073002f6
jaonjohnkim <kim.jaon79@gmail.com>
2017-08-21 18:09:36 -0700

    first two recusion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,42 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+//You should use :
+// document.body
+// element.childNodes
+// element.classList
+
+//document.body.childNodes : gives us the entire array of nodes
+//document.body.childNodes[index].classList : returns an array of the class names
+//document.body.childNodes[index].childNodes : if nested child nodes
+
+var getElementsByClassName = function(className) {
+  //input : string that specifies class
+  //output : an array of nodes that have the className
+  let output = [];
+  let doc = document.body.childNodes;
+
+
+  let classCheck = function (node) {
+    
+    if (node.classList) {
+      let nodeArray = Array.from(node.classList);
+      if (nodeArray.includes(className)) {
+        output.push(node);
+      }
+    }
+    
+    if (node.childNodes) {
+      for (let j = 0; j < node.childNodes.length; j++) {
+        classCheck(node.childNodes[j]);
+      }
+    }
+  };
+  
+  for (let i = 0; i < doc.length; i++) {
+    classCheck(doc[i]);
+  }
+  
+  return output;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,54 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  
+  //types = string, number, boolean, undefined, null
+
+  //arrays
+
+  //objects
+  if (typeof obj === 'string') {
+    return '"' + obj + '"'; 
+  }
+
+  if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+    return obj + '';
+  }
+  
+  
+
+  //
+  if ( Array.isArray(obj) ) {
+    if (obj.length === 0) {
+      return '[]';
+    }
+
+    let newArray = [];
+    for (var i = 0; i < obj.length; i++) {
+      newArray.push(stringifyJSON(obj[i]));
+    }
+
+    return '[' + newArray.join(',') + ']';
+  }
+  
+  if (typeof obj === 'object') {
+    if (Object.keys(obj).length === 0) {
+      return '{}';
+    }
+    
+    let newObj = [];
+    for (var key in obj) {
+      let keyStr = stringifyJSON(key);
+      let valueStr = stringifyJSON(obj[key]);
+      if (typeof valueStr === 'function' || typeof valueStr === 'undefined') {
+        continue;
+      }
+      newObj.push(keyStr + ':' + valueStr);
+      
+    }
+    return '{' + newObj.join(',') + '}';
+
+  }  //end of obj
+
+
 };
